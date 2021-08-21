5d0a43d7e9754beba4221230d9da13ee0b63ac4b
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-08-21 11:28:31 +0530

    display during delete updated

--- ll_header/del.h
+++ ll_header/del.h
@@ -1,11 +1,21 @@
 void del(node **h,short p)// list starts from 1
 {
     #include "get.h"
-    if((*h)->n==0 || p==1){
-    *h=(*h)->n;
-    return;
-    }
+    #include "disp_in.h"
+    printf("\n");
+     void (*f[4])(void *);
+    f[0]=dispi__19BIT0292;
+    f[1]=dispf__19BIT0292;
+    f[2]=disps__19BIT0292;
+    f[3]=dispc__19BIT0292;
     node *t;
+    if((*h)->n==0 || p==0 || p==1)
+    {
+        f[(*h)->dt]((*h)->d);
+        printf(" deleted");
+        (*h)=(*h)->n;
+        return;
+    }
     int l=len(h);
     if(p==-1 || l<=p)
     {
@@ -15,13 +25,6 @@ void del(node **h,short p)// list starts from 1
     {
         t=get(*h,p-1);
     }
-    #include "disp_in.h"
-    printf("\n");
-     void (*f[4])(void *);
-    f[0]=dispi__19BIT0292;
-    f[1]=dispf__19BIT0292;
-    f[2]=disps__19BIT0292;
-    f[3]=dispc__19BIT0292;
     f[t->n->dt](t->n->d);  
     printf(" deleted");
     t->n=t->n->n;
--- ll_header/get.h
+++ ll_header/get.h
@@ -2,7 +2,7 @@ node *get(node *h,short n)
 {
     node *p=h;
     short i=1;
-    while(p && p->n && n!=i++)//in case of -1 it will get excute uptil end
+    while(p->n && n!=i++)//in case of -1 it will get excute uptil end
     p=p->n;
     return p;    
 }
