1943322bc4714e82434da65df47e5163765e1421
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-08-21 11:13:42 +0530

    diplay added while insering head

--- ll_header/ins.h
+++ ll_header/ins.h
@@ -2,23 +2,25 @@ void in(node **h,void *a,short d,short n)//-1 or any element greater than list l
 {
     #include "node_form.h"
     #include "get.h"
+     void (*f[4])(void *);
+     #include "disp_in.h"
+    f[0]=dispi__19BIT0292;
+    f[1]=dispf__19BIT0292;
+    f[2]=disps__19BIT0292;
+    f[3]=dispc__19BIT0292;
+    printf("\n");
     if(!*h || d==0)
     {
         node* t=*h;
         *h=node_form(a,n);
         (*h)->n=t;
+        f[(*h)->dt]((*h)->d);  
+        printf(" inserted");
         return;
     }
     node *p=get(*h,d),*t=node_form(a,n); 
     t->n=p->n;
     p->n=t;
-    #include "disp_in.h"
-    printf("\n");
-     void (*f[4])(void *);
-    f[0]=dispi__19BIT0292;
-    f[1]=dispf__19BIT0292;
-    f[2]=disps__19BIT0292;
-    f[3]=dispc__19BIT0292;
-    f[p->n->dt](p->n->d);  
+    f[t->dt](t->d);  
     printf(" inserted");
 }
